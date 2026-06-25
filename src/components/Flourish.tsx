c4ebd756990bab9fc1ebf6ca1097243fb409c648
Doug McDonald <dougajmcdonald@gmail.com>
2026-06-25 11:33:40 +0100

    Add flourish

---
+++ src/components/Flourish.tsx
@@ -0,0 +1,10 @@
+export function Flourish() {
+  return (
+    <div className="ml-2 flex items-center gap-2">
+      <div className="border-primary-600 flex size-36 items-center justify-center rounded-full border-10">
+        <div className="border-primary-200 size-20 rounded-full border-8"></div>
+      </div>
+      <div className="border-l-primary-600 size-0 border-10 border-transparent"></div>
+    </div>
+  )
+}
--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,11 +1,15 @@
 import { Heading } from '@components/Heading'
 import { Text } from '@components/Text'
+import { Flourish } from '@components/Flourish'
 
 export function Header() {
   return (
-    <section className="flex w-full flex-col items-center justify-center gap-6 pt-20 pb-14">
-      <Heading>Doug McDonald</Heading>
-      <Text className="tracking-wide">Product Designer & Design Engineer</Text>
+    <section className="flex w-full flex-col items-center justify-center gap-14 pt-20 pb-14">
+      <Flourish />
+      <div className="flex flex-col items-center justify-center gap-6">
+        <Heading>Doug McDonald</Heading>
+        <Text className="tracking-wide">Product Designer & Design Engineer</Text>
+      </div>
     </section>
   )
 }
