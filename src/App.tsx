5b70cb45db2624cb218d4cab724ebd1983cff1bb
Doug McDonald <dougajmcdonald@gmail.com>
2026-06-25 23:42:51 +0100

    Add footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router'
 import { Wrapper } from '@layout/Wrapper'
 import { Header } from '@layout/Header'
 import { Navigation } from '@layout/Navigation'
+import { Footer } from '@layout/Footer'
 import { Home } from '@pages/Home'
 import { About } from '@pages/About'
 import { Work } from '@pages/Work'
@@ -16,6 +17,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/work" element={<Work />} />{' '}
       </Routes>
+      <Footer />
     </Wrapper>
   )
 }
---
+++ src/layout/Footer.tsx
@@ -0,0 +1,23 @@
+import { GithubLogoIcon, EnvelopeIcon } from '@phosphor-icons/react'
+import { Link } from 'react-router'
+
+const links = [
+  { url: 'https://www.dougmcdonald.co.uk', label: 'dougmcdonald.co.uk © 2026' },
+  { url: 'https://github.com/dougajmcdonald', label: '', icon: GithubLogoIcon },
+  { url: 'mailto:dougajmcdonald@gmail.com', label: '', icon: EnvelopeIcon },
+]
+
+export function Footer() {
+  return (
+    <footer className="border-primary-200 mt-6 flex min-h-12 w-full items-center justify-center border-t">
+      <ul className="my-8 flex gap-12">
+        {links.map((link) => (
+          <Link to={link.url}>
+            {link.icon && <link.icon className="text-primary-500 size-6" />}
+            {link.label && <span className="text-primary-600">{link.label}</span>}
+          </Link>
+        ))}
+      </ul>
+    </footer>
+  )
+}
